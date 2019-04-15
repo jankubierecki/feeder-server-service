9f60706017f5d75991d25ffe5a25c0e875b86d32
jankubierecki <jankubierecki@gmail.com>
2019-04-15 18:25:34 +0200

    service_connection

--- run.py
+++ run.py
@@ -49,9 +49,9 @@ def service_connection(key, mask):
             sock.close()
     if mask & selectors.EVENT_WRITE:
         if data.outb:
+            print(data.outb)
             print('echoing', repr(data.outb), 'to', data.addr)
             sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
 
 
 HOST = os.environ.get('SERVER_HOST', '')
