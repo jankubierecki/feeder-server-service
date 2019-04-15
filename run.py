5f3d5058e3ce8240255548f4699eb5968509a178
jankubierecki <jankubierecki@gmail.com>
2019-04-15 14:57:00 +0200

    multi-connections server

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
+import types
 
 from sshtunnel import SSHTunnelForwarder
 import pymongo
+
 import socket
+import selectors
 
 
 def connect_mongo():
@@ -24,21 +27,51 @@ def connect_mongo():
     return client
 
 
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print('accepted connection from', addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print('closing connection to', data.addr)
+            sel.unregister(sock)    # client closed connection so the server
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print('echoing', repr(data.outb), 'to', data.addr)
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+
 HOST = os.environ.get('SERVER_HOST', '')
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    """ new unique socket object, used to talk with client 
-    and it’s distinct from the listening socket that the server is using to accept new connections """
+sel = selectors.DefaultSelector()
 
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
-            print(data)
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((HOST, PORT))
+lsock.listen()
+
+print('listening on', (HOST, PORT))
+
+lsock.setblocking(False)  # calls made to this socket will no longer block
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+while True:
+    events = sel.select(timeout=None)  # returns a list of (key, events) tuples, one for each socket
+    for key, mask in events:
+        if key.data is None:
+            accept_wrapper(key.fileobj)  # call to get the new socket object and register it with the selector
+        else:
+            service_connection(key, mask)  # client socket already been accepted, so service it
