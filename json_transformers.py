d6945f2ca5c088ff03c244f7f1967d0ab3958209
jankubierecki <jankubierecki@gmail.com>
2019-04-15 21:44:54 +0200

    fixes service connection

---
+++ json_transformers.py
@@ -0,0 +1,13 @@
+import json
+
+
+def binary_to_dict(b):
+    jsn = ''.join(chr(int(x, 2)) for x in b.split())
+    d = json.loads(jsn)
+    return d
+
+
+def dict_to_binary(self, d):
+    s = json.dumps(d)
+    binary = ' '.join(format(ord(letter), 'b') for letter in s)
+    return str.encode(binary)
